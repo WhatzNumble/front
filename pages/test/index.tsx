f2e83d6878753e2a370882c4bb715d7505701348
pjainxido <pjainxido@gmail.com>
2022-04-18 17:55:01 +0900

    Add test page Layout

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useSelector, useDispatch } from "react-redux";
 import { AppState } from "store/index";
 import { testActions } from "store/test";
+import Layout from "components/Layout";
 
 import Video from "components/Video";
 
@@ -14,18 +15,20 @@ const Test: NextPage = () => {
   };
 
   return (
-    <div className='Test'>
-      <h1 className='title'>테스트에요~</h1>
-      <button onClick={onClick}>{count}</button>
-      <style jsx>{`
-        .Test {
-          .title {
-            color: blue;
+    <Layout>
+      <div className='Test'>
+        <h1 className='title'>테스트에요~</h1>
+        <button onClick={onClick}>{count}</button>
+        <style jsx>{`
+          .Test {
+            .title {
+              color: blue;
+            }
           }
-        }
-      `}</style>
-      <Video isEmbed videoSrc='uwlXQRWu54E' />
-    </div>
+        `}</style>
+        <Video isEmbed videoSrc='uwlXQRWu54E' />
+      </div>
+    </Layout>
   );
 };
 
