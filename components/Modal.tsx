673bfd22b0d74865248b1968195c579705d4570b
kwangsun <eyeyel@naver.com>
2022-04-21 14:33:48 +0900

    feat: Modal.tsx 추가 후 Confirm.tsx에 적용

---
+++ components/Modal.tsx
@@ -0,0 +1,58 @@
+import {CSSTransition} from 'react-transition-group';
+import ReactDOM from 'react-dom';
+import {useEffect, useState} from 'react';
+
+interface Props {
+    children: React.ReactNode
+    show: boolean
+    transitionTime?: number
+}
+
+function Modal({children, show, transitionTime = 200}: Props){
+    const [hasWin, setHasWin] = useState(false);
+    useEffect(()=>{
+        setHasWin(true);
+    }, []);
+
+    return (
+        <>
+            {children}
+            {ReactDOM.createPortal(
+                <CSSTransition
+                    in={show}
+                    timeout={200}
+                    classNames="overlay"
+                    unmountOnExit
+                >
+                    <div className='overlay'>
+                        <style jsx>{`
+                            .overlay {
+                                position: fixed;
+                                width: 100%;
+                                height: 100%;
+                                background-color: rgba(0,0,0,0.3);
+                            }
+                    
+                            .overlay-enter {
+                                opacity: 0;
+                            }
+
+                            .overlay-enter-active {
+                                opacity: 1;
+                                transition: ${transitionTime}ms;
+                            }
+
+                            .overlay-exit-active {
+                                opacity: 0;
+                                transition: ${transitionTime}ms;
+                            }
+                        `}</style>
+                    </div>
+                </CSSTransition>,
+                document.querySelector('#modal-root')!
+            )}
+        </>
+    )
+}
+
+export default Modal;
