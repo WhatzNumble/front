45d1cc02974299a8ff301d48f2f686bcc614931d
kwangsun <eyeyel@naver.com>
2022-04-22 21:33:53 +0900

    modify: Toast 컴포넌트 개선

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import {useDispatch} from 'react-redux';
-import {uiActions} from 'store/ui';
+import { CSSTransition } from 'react-transition-group';
 
 interface Props {
     message?: string | number
@@ -9,55 +8,64 @@ interface Props {
 }
 
 function Toast({message = '', duration = 3}: Props){
-    const dispatch = useDispatch();
     const [hasWin, setHasWin] = useState(false);
+    const [show, setShow] = useState(true);
 
     useEffect(()=>{
         setHasWin(true);
         setTimeout(()=> {
-            dispatch(uiActions.removeToast());
+            setShow(false);
         }, duration * 1000);
     }, []);
 
     return hasWin ? <>
         {ReactDOM.createPortal(
-            <div className="Toast">
-                {message}
-                <style jsx>{`
-                    .Toast {
-                        z-index: 99;
-                        position: fixed;
-                        top: 80%;
-                        left: 50%;
-                        padding: 10px 15px;
-                        background-color: #00000083;
-                        color: white;
-                        font-size: 12px;
-                        border-radius: 30px;
-                        animation: in ${duration}s;
-                        animation-fill-mode: forwards;
-                    }
+            <CSSTransition
+                in={show}
+                timeout={200}
+                classNames="toast"
+                unmountOnExit
+            >
+                <div className="Toast">
+                    {message}
+                    <style jsx>{`
+                        .Toast {
+                            z-index: 99;
+                            position: fixed;
+                            top: 80%;
+                            left: 50%;
+                            padding: 10px 15px;
+                            background-color: #00000083;
+                            color: white;
+                            font-size: 12px;
+                            border-radius: 30px;
+                        }
 
-                    @keyframes in {
-                        0% {
+                        .toast-enter {
                             opacity: 0;
                             transform: translate(-50%, 0);
                         }
-                        10% {
+                        .toast-enter-active {
+                            opacity: 1;
+                            transform: translate(-50%, -20px);
+                            transition: .2s;
+                        }
+                        .toast-enter-done {
                             opacity: 1;
                             transform: translate(-50%, -20px);
                         }
-                        90% {
+                        .toast-exit {
                             opacity: 1;
                             transform: translate(-50%, -20px);
                         }
-                        100% {
+                        .toast-exit-active {
                             opacity: 0;
                             transform: translate(-50%, 0);
+                            transition: .2s;
                         }
-                    }
-                `}</style>
-            </div>,
+                    `}</style>
+                </div>
+            </CSSTransition>,
             document.querySelector('#modal-root')!
         )}
     </> : <></>;
