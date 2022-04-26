85d62583940cb9d71e6df027dfe13adb615a0a8b
kwangsun <eyeyel@naver.com>
2022-04-26 17:07:32 +0900

    Kwangsun (#17)
    * feat: 마이 비디오 페이지 추가
    
    * feat : 업로드 화면 추가, 레이아웃 수정
    
    * modify : 마이비디오 -> 임베드, 직접 영상 업로드 페이지 틀작업 완료

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,27 +1,71 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import TabBar from "./TabBar";
+
+export const TAB_HEIGHT = 56;
 
 interface Props {
     children: React.ReactNode
     title?: string
+    headerTitle?: string
     hasTabBar?: boolean
 }
 
-function Layout({children, title = '', hasTabBar = true}: Props){
+function Layout({children, title = '', headerTitle, hasTabBar = true}: Props){
+    const router = useRouter();
+
+    const onClickBack = ()=>{
+        router.back();
+    }
+
     return (
-        <div>
+        <div className="Layout">
             <Head>
                 <title>{title ? `${title} | Whatz` : 'Whatz'}</title>
             </Head>
-            <main className="Layout">
-                {children}
+            <main className="main">
+                {headerTitle && 
+                    <header className="header">
+                        {headerTitle}
+                        <button className="back" onClick={onClickBack}>◀뒤로</button>
+                    </header>
+                }
+                <div className="content">
+                    {children}
+                </div>
             </main>
-            {hasTabBar && <TabBar/>}
+            {hasTabBar && <TabBar height={TAB_HEIGHT}/>}
             <style jsx>{`
                 .Layout {
                     height: 100vh;
+                }
+
+                .header {
                     display: flex;
                     justify-content: center;
+                    align-items: center;
+                    width: 100%;
+                    height: 35px;
+                    font-weight: bold;
+                    font-size: 13px;
+                    background-color: #ebe9e9;
+                }
+
+                .back {
+                    position: absolute;
+                    left: 15px;
+                    font-weight: bold;
+                }
+
+                .main {
+                    display: flex;
+                    flex-direction: column;
+                    width: 100%;
+                    height: calc(100vh - ${TAB_HEIGHT}px);
+                }
+
+                .content {
+                    flex-grow: 1;
                 }
             `}</style>
         </div>
--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,20 +1,27 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const LINK_INFOS = [
     {path: '/', name: '홈'},
-    {path: '/test2', name: '마이 비디오'},
-    {path: '/interest', name: '관심 영상'},
+    {path: '/myVideo', name: '마이 비디오'},
+    {path: '/like', name: '관심 영상'},
     {path: '/profile', name: '프로필'},
 ];
 
-function TabBar(){
+interface Props {
+    height?: number;
+}
+
+function TabBar({height = 56}: Props){
+    const router = useRouter();
+
     return (
         <nav className="TabBar">
             <ul className="nav-box">
                 {LINK_INFOS.map(info => (
                     <li key={info.path}>
                         <Link href={info.path}>
-                            <a>{info.name}</a>
+                            <a className={router.pathname === info.path ? 'match' : ''}>{info.name}</a>
                         </Link>
                     </li>
                 ))}
@@ -32,12 +39,18 @@ function TabBar(){
                 .nav-box {
                     display: flex;
                     justify-content: center;
+                    align-items: center;
+                    height: ${height}px;
                 }
 
                 li {
-                    padding: 15px 15px;
+                    padding: 0 15px;
                     font-size: 10px;
                     color: gray;
+                }
+
+                .match {
+                    color: red;
                 }
             `}</style>
         </nav>
