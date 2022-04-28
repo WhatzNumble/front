34c8c564c75e4db2b91c4710601ff3b01d4c16c9
pjainxido <pjainxido@gmail.com>
2022-04-28 22:08:49 +0900

    Add redirect when isLoggedIn in RedirectPage

--- pages/oauth/redirect.tsx
+++ pages/oauth/redirect.tsx
@@ -1,20 +1,31 @@
 import { useEffect } from 'react';
-import axios from 'axios';
-import { NextPage } from 'next';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { userActions } from 'store/user';
 import { useRouter } from 'next/router';
+import { AppState } from 'store';
+import axios from 'axios';
 
 const OauthRedirectPage = () => {
-  const dispatch = useDispatch();
   const router = useRouter();
   const { token } = router.query; // redirect uri의 쿼리스트링에 있는 access token
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state: AppState) => state.user);
 
   useEffect(() => {
+    console.log(token);
     if (typeof token === 'string') {
-      dispatch(userActions.setToken({ token: token }));
+      dispatch(userActions.login({ token: token, socialType: 'kakao' }));
+      const response = axios.get('http://localhost:8080/get/user');
+      console.log(response)
     }
-  }, []);
+  }, [token]);
+
+  useEffect(() => {
+    if(isLoggedIn){
+      router.push('/');
+    }
+  }, [isLoggedIn]);
+
   return <div>Loading...</div>;
 };
 
