ad78ea52b28c9afeba40aed51b4bb1d62aeaf98f
Park Jein <pjainxido@gmail.com>
2022-04-29 15:54:24 +0900

    Feat: onBoarding page (#19)
    - index 페이지에 접속시 local storage에 onBoarded-Whatz 키를 가진 값이 없으면 onBoarding 페이지로 redirect
    - 온보딩 페이지에서 일정시간 이후 다시 메인페이지로 redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
-import Layout from 'components/Layout'
-import type { NextPage } from 'next'
+import Layout from 'components/Layout';
+import type { NextPage } from 'next';
+import Router from 'next/router';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
+  const detectFirstView = () => {
+    const localStorageKey = 'onBoarded-Whatz'
+    const isOnboared = localStorage.getItem(localStorageKey);
+    localStorage.setItem(localStorageKey, 'true');
+    return !!isOnboared;
+  };
+
+  useEffect(() => {
+    if (!detectFirstView()) {
+      Router.push('/onboarding');
+    }
+  }, []);
+
   return (
     <Layout title='홈'>
-      <div>
-        홈
-      </div>
+      <div>홈</div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ pages/onboarding/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import { NextPage } from 'next';
+import Router from 'next/router';
+import Image from 'next/image';
+
+const onBoardingPage: NextPage = () => {
+  const redirectSecond = 3;
+
+  useEffect(() => {
+    setTimeout(() => {
+      Router.push('/');
+    }, redirectSecond * 1000);
+  }, [redirectSecond]);
+
+  return (
+    <div className='onBoardPage'>
+      <div className='logoContainer'>
+        <Image src='/logo.svg' width={44.54} height={40.15} alt='whatz-icon' />
+        <Image src='/whatz.svg' width={114.46} height={25.63} alt='whatz-logo' />
+      </div>
+      <style jsx>
+        {`
+          .logoContainer {
+            position: absolute;
+            width: 180px;
+            height: 40px;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            display: flex;
+            justify-content: space-around;
+          }
+          .onBoardPage {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+            background: #ffffff;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default onBoardingPage;
