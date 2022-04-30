bc0a995d72724157cf0fe5ba2fc77871373a8f4b
pjainxido <pjainxido@gmail.com>
2022-04-30 20:02:02 +0900

    Update useUserTypeRedirect.ts

--- hooks/useUserTypeRedirect.ts
+++ hooks/useUserTypeRedirect.ts
@@ -1,17 +1,27 @@
 import { AppState } from 'store';
 import Router from 'next/router';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+import { uiActions } from 'store/ui';
 
 type userType = 'user' | 'guest';
 
 const useUserTypeRedirect = (href: string, redirectUserType: userType) => {
+  const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state: AppState) => state.user);
   useEffect(() => {
     const isUser = redirectUserType === 'user';
     console.log(isLoggedIn, isUser);
-    if (isUser === isLoggedIn) Router.push(href);
-  }, [redirectUserType, href, isLoggedIn]);
+    if (isUser === isLoggedIn) {
+      Router.push(href);
+      dispatch(
+        uiActions.pushToast({
+          message: isUser ? '잘못된 접근입니다.' : '로그인이 필요한 서비스입니다.',
+        })
+      );
+    }
+  }, [dispatch, redirectUserType, href, isLoggedIn]);
 };
 
 export default useUserTypeRedirect;
