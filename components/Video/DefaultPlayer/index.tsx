a9bd67819000d8e9a4b48f2091b20f18fc4d8aa3
pjainxido <pjainxido@gmail.com>
2022-05-03 18:44:25 +0900

    Remove progress bar, add mute option player

--- components/Video/DefaultPlayer/index.tsx
+++ components/Video/DefaultPlayer/index.tsx
@@ -86,6 +86,7 @@ const DefaultPlayer: React.FC<Props> = ({ videoSrc, active }) => {
       <ReactHlsPlayer
         className='DefaultPlayer'
         playerRef={playerRef}
+        muted={mute}
         width='100%'
         height='100%'
         loop
