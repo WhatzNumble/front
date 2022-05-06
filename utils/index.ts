25d4a8a80be085788d548dc2524c21e85b2335e2
pjainxido <pjainxido@gmail.com>
2022-05-06 09:56:25 +0900

    Update util cookie functions

--- utils/index.ts
+++ utils/index.ts
@@ -4,6 +4,17 @@ const getCookie = (name: string) => {
   });
 };
 
+function getCookie2(name: string) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
 const deleteCookie = (name: string, path?: string, domain?: string) => {
   console.log(getCookie(name));
   if (getCookie(name)) {
@@ -13,7 +24,7 @@ const deleteCookie = (name: string, path?: string, domain?: string) => {
         '=' +
         (path ? ';path=' + path : '') +
         (domain ? ';domain=' + domain : '') +
-        ';expires=Thu, 01 Jan 1970 00:00:01 GMT'
+        '; Expires=Thu, 01 Jan 1970 00:00:01 GMT'
     );
 
     document.cookie =
@@ -21,7 +32,7 @@ const deleteCookie = (name: string, path?: string, domain?: string) => {
       '=' +
       (path ? ';path=' + path : '') +
       (domain ? ';domain=' + domain : '') +
-      ';expires=Thu, 01 Jan 1970 00:00:01 GMT';
+      '; Expires=Thu, 01 Jan 1970 00:00:01 GMT';
   }
 };
 
