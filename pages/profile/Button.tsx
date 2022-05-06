2800fcca5ab1be8e41d178ae6662bdcb323ec43e
pjainxido <pjainxido@gmail.com>
2022-05-06 14:09:29 +0900

    Add profile Button

--- pages/profile/Button.tsx
+++ pages/profile/Button.tsx
@@ -1,28 +1,45 @@
 interface Props {
   onClick: () => void;
   buttonColor?: string;
-  buttonTheme?: 'light' | 'dark';
+  theme?: 'light' | 'dark';
   text: string;
   textColor?: string;
-  width: string;
+  width?: string;
 }
-const Button: React.FC<Props> = ({ onClick, width, text, buttonColor, textColor = '#000' }) => {
-  const backgroundColor = buttonColor ? buttonColor : '#D8FF69';
+
+const Button: React.FC<Props> = ({
+  onClick,
+  theme = 'light',
+  width,
+  text,
+  buttonColor,
+  textColor = '#000',
+}) => {
+  const backgroundColor = buttonColor ? buttonColor : theme === 'light' ? '#D8FF69' : '#000';
   return (
-    <button className='Button' onClick={onClick}>
-      {text}
+    <div className='wrapper'>
+      <button className='button' onClick={onClick}>
+        {text}
+      </button>
       <style jsx>
         {`
-          .Button {
-            width: ${width};
+          .wrapper {
+            margin: 4px 0;
+          }
+          .button {
+            width: ${width || 'auto'};
             height: 48px;
             background: ${backgroundColor};
-            border-radius: 28px;
-            color: ${textColor};
+            border-radius: 30px;
+            color: ${theme === 'dark' ? '#fff' : textColor};
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 140%;
+            ${theme === 'dark' && 'border: 1px solid #8F8F8F;'}
           }
         `}
       </style>
-    </button>
+    </div>
   );
 };
 
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -9,12 +9,14 @@ import { userActions } from 'store/user';
 import useUserTypeRedirect from 'hooks/useUserTypeRedirect';
 import { deleteCookie } from 'utils';
 import useUserState from 'hooks/useUserState';
+import Button from './Button';
 
 import ProfileAvatar from './ProfileAvatar';
 
 const profileWrapper = css`
   .profileWrapper {
     margin-top: 168px;
+    padding: 0px 32px;
   }
 `;
 
@@ -52,22 +54,30 @@ const ProfilePage = () => {
         <div className='avatar'>
           <ProfileAvatar avatarSrc={userAvatar} />
         </div>
-        <div>{`email: ${userEmail}`}</div>
-        <div>{`nick: ${nickName}`}</div>
+        <div className='nameWrapper'>
+          <div>{`email: ${userEmail}`}</div>
+          <div>{`nick: ${nickName}`}</div>
+        </div>
+        <div className='buttonWrapper'>
+          <Button onClick={handleLogout} width={'100%'} text='프로필 편집'  />
+          <Button onClick={handleLogout} width={'100%'} text='로그아웃' theme='dark'/>
+          <Button onClick={handleLogout} width={'100%'} text='탈퇴하기' theme='dark' />
+        </div>
         <Link href={'/profile/edit'}>프로필 수정</Link>
         <Link href={{ pathname: '/profile/edit', query: { editType: 'signup' } }}>프로필 생성</Link>
-        <button onClick={() => handleLogout()}>log out</button>
       </div>
       <style jsx>{profileWrapper}</style>
       <style jsx>
         {`
-        .avatar{
-          display: flex;
-          justify-content: center;
-        }
-          
+          .avatar {
+            display: flex;
+            justify-content: center;
+          }
+          .buttonWrapper {
+            width: fit-contents;
+            margin: 0 auto;
+          }
         `}
-
       </style>
     </Layout>
   );
