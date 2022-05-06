20d412d9bb79010f5def55b61f7a0346f9971ab0
pjainxido <pjainxido@gmail.com>
2022-05-06 09:58:14 +0900

    Fix Profile page request api uri

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -2,30 +2,37 @@ import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import Link from 'next/link';
 import Router from 'next/router';
+import css from 'styled-jsx/css';
 
-import config from 'utils/config';
 import Layout from 'components/Layout';
 import { userActions } from 'store/user';
 import useUserTypeRedirect from 'hooks/useUserTypeRedirect';
 import { deleteCookie } from 'utils';
 import useUserState from 'hooks/useUserState';
 
+import ProfileAvatar from './ProfileAvatar';
+
+const profileWrapper = css`
+  .profileWrapper {
+    margin-top: 168px;
+  }
+`;
+
 const ProfilePage = () => {
-  useUserTypeRedirect('/', 'guest');
+  // useUserTypeRedirect('/', 'guest');
   const dispatch = useDispatch();
   const { userEmail, nickName, userAvatar, token } = useUserState();
 
   const logoutRequest = () => {
     console.log(token);
     axios
-      .post('/api/logout', {
+      .post('/member/logout', {
         //post 인데 명세서에 요청 데이터가 없어 일단 비워둠
       })
       .then((response) => {
-        console.log(response);
-        //로그인 성공시 dispatch
+        console.log(response.data);
+        //로그아웃 성공시 dispatch
         axios.defaults.headers.common['x-auth-token'] = '';
-        dispatch(userActions.logout());
       })
       .catch((error) => {
         console.error(error);
@@ -33,23 +40,35 @@ const ProfilePage = () => {
   };
 
   const handleLogout = () => {
-    deleteCookie('access-token', '/', config.hostDomain);
     logoutRequest();
-    Router.push('/');
+    deleteCookie('access-token', '/');
     dispatch(userActions.logout());
+    Router.push('/');
   };
 
   return (
     <Layout>
-      <div>
-        user info // 리덕스 login action 에 지정된 데이터 ,ap login response 데이터 준비되면 수정
+      <div className='profileWrapper'>
+        <div className='avatar'>
+          <ProfileAvatar avatarSrc={userAvatar} />
+        </div>
         <div>{`email: ${userEmail}`}</div>
         <div>{`nick: ${nickName}`}</div>
-        <div>{`avatarSrc: ${userAvatar}`}</div>
+        <Link href={'/profile/edit'}>프로필 수정</Link>
+        <Link href={{ pathname: '/profile/edit', query: { editType: 'signup' } }}>프로필 생성</Link>
+        <button onClick={() => handleLogout()}>log out</button>
       </div>
-      <Link href={'/profile/edit'}>프로필 수정</Link>
-      <Link href={{ pathname: '/profile/edit', query: { editType: 'signup' } }}>프로필 생성</Link>
-      <button onClick={() => handleLogout()}>log out</button>
+      <style jsx>{profileWrapper}</style>
+      <style jsx>
+        {`
+        .avatar{
+          display: flex;
+          justify-content: center;
+        }
+          
+        `}
+
+      </style>
     </Layout>
   );
 };
