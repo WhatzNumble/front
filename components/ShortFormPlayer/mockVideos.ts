25f3936cd5db4fc2e6cae841fe8b51a8569f601d
pjainxido <pjainxido@gmail.com>
2022-05-10 20:39:11 +0900

    feat: Add mockVideos

---
+++ components/ShortFormPlayer/mockVideos.ts
@@ -0,0 +1,79 @@
+import { Video } from 'libs/types';
+
+const detailMockString: string = `
+1. Use gridlines to balance your shot.
+2. Set your camera's focus.
+3. Focus on one subject.
+4. Embrace negative space.
+5. Find different perspectives.
+6. Play with reflections.
+7. Use leading lines.
+8. Look for symmetry.
+9. Keep an eye out for repetitive patterns.
+10. Play around with color blocking.
+11. Avoid zooming in.
+12. Capture small details.
+13. Use natural light.
+14. If you use flash, only do so during the day.
+`;
+
+const mockVideos: Video[] = [
+  {
+    videoId: 123123,
+    nickname: 'user1',
+    profile: '/profile.png',
+    likes: 5,
+    title: 'title1',
+    content: detailMockString,
+    videoDate: '2022-05-06T12:07:35.052847',
+    views: 20,
+    videoThumbnail: '/profile.png',
+    directDir:
+      'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+    embedLink: null,
+  },
+  {
+    videoId: 123123,
+    nickname: 'user1',
+    profile: '/profile.png',
+    likes: 5,
+    title: 'title1',
+    content: detailMockString,
+    videoDate: '2022-05-06T12:07:35.052847',
+    views: 20,
+    videoThumbnail: '/profile.png',
+    directDir:
+      'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+    embedLink: null,
+  },
+  {
+    videoId: 123123,
+    nickname: 'user1',
+    profile: '/profile.png',
+    likes: 5,
+    title: 'title1',
+    content: detailMockString,
+    videoDate: '2022-05-06T12:07:35.052847',
+    views: 20,
+    videoThumbnail: '/profile.png',
+    directDir:
+      'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+    embedLink: null,
+  },
+  {
+    videoId: 123123,
+    nickname: 'user1',
+    profile: '/profile.png',
+    likes: 5,
+    title: 'title1',
+    content: detailMockString,
+    videoDate: '2022-05-06T12:07:35.052847',
+    views: 20,
+    videoThumbnail: '/profile.png',
+    directDir:
+      'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+    embedLink: 'https://www.youtube.com/watch?v=_whaAD__3vI',
+  },
+];
+
+export default mockVideos;
