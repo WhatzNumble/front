816c9674b4cf79669cb85a3ce805af947d02c8ec
pjainxido <pjainxido@gmail.com>
2022-05-13 16:02:09 +0900

    feat: add gaTrackingID on config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -2,6 +2,7 @@ const config = {
   hostDomain: process.env.NEXT_PUBLIC_ENV_HOST_DOMAIN,
   hostURL: process.env.NEXT_PUBLIC_ENV_HOST_URL,
   apiBaseURL: process.env.NEXT_PUBLIC_ENV_API_URL,
+  gaTrackingID: process.env.NEXT_PUBLIC_ENV_GA_TRACKING_ID,
   authHeaderKey: 'x-auth-token',
   cookieAuthHeaderKey: 'access-token',
 };
