a09e891d3181cdefb27140cd29f294822b1f856f
pjainxido <pjainxido@gmail.com>
2022-05-14 20:35:27 +0900

    refactor: loginButton

--- pages/login/LoginButton.tsx
+++ pages/login/LoginButton.tsx
@@ -1,20 +1,42 @@
+import Image from 'next/image';
+
 interface ISocialLoginButton {
-  label: string;
+  text: string;
+  logoSrc?: string;
+  backgroundColor?: string;
+  color?: string;
   hrefLink: string;
 }
 
-const LoginButton: React.FC<ISocialLoginButton> = ({ label, hrefLink }) => {
+const LoginButton: React.FC<ISocialLoginButton> = ({
+  logoSrc = '/icon/logo/whatz.svg',
+  backgroundColor = 'var(--primary)',
+  text,
+  hrefLink,
+}) => {
   return (
     <a href={hrefLink}>
-      <button className='SocialLoginButton'>{label}</button>
+      <button className='loginButton'>
+        <label>
+          <Image src={logoSrc} width={19} height={19} alt='login logo' />
+          <p>{text}</p>
+        </label>
+      </button>
       <style jsx>
         {`
-          .SocialLoginButton {
+          .loginButton {
             width: 311px;
             height: 48px;
-            background: #565656;
+            background: ${backgroundColor};
             border-radius: 28px;
-            color: #fff;
+            color: var(--black);
+            label {
+              display: flex;
+              justify-content: center;
+              p{
+                margin-left: 10px;
+              }
+            }
           }
         `}
       </style>
